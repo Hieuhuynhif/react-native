9e61cb898b61242350af3fb1a2851f3f4f55c3dd
Hieu Huynh <hieuhuynhif@gmail.com>
2023-04-19 15:38:31 +0700

    19/04/2023

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 import express from 'express';
 import {createCustomToken} from './pushNotifyModule.js';
-// import fs from 'fs'
-// import https from 'https'
-// const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-// const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-
-// const credentials = {key: privateKey, cert: certificate};
+import admin from 'firebase-admin';
 const app = express();
- 
-// your express configuration here
-
-app.use(express.static("D:/Workspace/React/react-native/server/app"))
+app.listen(3000, ()=>{
+    console.log("Http-server is running on port 3000");
+})
+app.use(express.static("D:/React/server/app"))
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
 app.get("/", (req, res)=>{
-    res.sendFile("D:/Workspace/React/react-native/server/app" + "/home.html")
+    res.sendFile("D:/React/server/app" + "/home.html")
 })
 app.post("/pushnotify", async(req,res)=>{
     let data = req.body;
@@ -26,14 +20,29 @@ app.post("/pushnotify", async(req,res)=>{
     if((userName == "web" && passWord == "web") || (userName == "mobile" && passWord == "mobile"))
     {
         let customToken = await createCustomToken(userName);
+        res.json({token:customToken});
+        if(data.touser)
+        {
+            const tokenTouser = await admin.database()
+            .ref(`users/${data.touser}`)
+            .once('value')
+            .then(snapshot => {
+              console.log('User data: ', snapshot.val());
+              return snapshot.val().token;
+            });
 
-        res.json({token:customToken})
+            await admin.messaging().sendMulticast({
+                tokens: [
+                    tokenTouser
+                ],
+                notification: {
+                  title: data.title,
+                  body: data.body,
+                },
+              });
+        }
     }
     else{
         res.json({token: "error"})
     }
 })
-// const httpsServer = https.createServer(credentials, app);
-app.listen(3000, ()=>{
-    console.log("Https-server is running on port 3000")
-});     
